efe90cde738e90f5a0aef2188b6a9bfe3f933eba
Ansh Singh <anshsingh200516@Anshs-MacBook-Pro-7.local>
2025-04-19 00:48:39 +0530

    adding application v5

--- client/src/pages/Calendar.tsx
+++ client/src/pages/Calendar.tsx
@@ -133,12 +133,12 @@ export default function Calendar() {
         </div>
         
         <div className="relative">
-          <input 
+          {/* <input 
             type="text" 
             placeholder="Search events..." 
             className="px-4 py-2 border border-gray-300 rounded-md w-full sm:w-64 focus:ring-primary-500 focus:border-primary-500"
-          />
-          <svg 
+          /> */}
+          {/* <svg 
             xmlns="http://www.w3.org/2000/svg" 
             className="h-5 w-5 text-gray-400 absolute right-3 top-2.5" 
             fill="none" 
@@ -151,7 +151,7 @@ export default function Calendar() {
               strokeWidth={2} 
               d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
             />
-          </svg>
+          </svg> */}
         </div>
       </div>
       
